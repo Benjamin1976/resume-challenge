e1f9bc14c813a41fefdc76f2eaf673ccbd707226
Benjamin <bp_coding@yahoo.com.au>
2024-05-01 19:24:04 +0800

    re-pushing

--- src/cloudprovider.js
+++ src/cloudprovider.js
@@ -1,11 +1,9 @@
 const cloud = "aws";
 const awsURLs = [
-  "https://6u7p0s026e.execute-api.us-east-1.amazonaws.com/dev",
-  "https://6u7p0s026e.execute-api.us-east-1.amazonaws.com/dev",
+  "https://72dsvl5pfh.execute-api.us-east-1.amazonaws.com/dev",
+  "https://72dsvl5pfh.execute-api.us-east-1.amazonaws.com/dev/",
 ];
 const azureURLs = [
-  "https://k7ib9cxbs4.execute-api.us-east-1.amazonaws.com/test/DynamoDBManager",
-  "https://bens-resume-challenge-api.azure-api.net/bens-resume-challenge-http-api?name=Weanie",
   "https://bens-resume-challenge-visits.azurewebsites.net/api/get-visits",
   "https://bens-resume-challenge-visits.azurewebsites.net/api/post-visits",
 ];
