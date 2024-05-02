ce09336645dd9ea5bdcc8bd43b456ea7c707c950
Benjamin <bp_coding@yahoo.com.au>
2024-05-02 22:03:45 +0800

    update url

--- src/cloudprovider.js
+++ src/cloudprovider.js
@@ -1,7 +1,7 @@
 const cloud = "aws";
 const awsURLs = [
-  "https://ggru6yy316.execute-api.us-east-1.amazonaws.com/dev/",
-  "https://ggru6yy316.execute-api.us-east-1.amazonaws.com/dev/",
+  "https://asqrkxqdcf.execute-api.us-east-1.amazonaws.com/dev/",
+  "https://asqrkxqdcf.execute-api.us-east-1.amazonaws.com/dev/",
 ];
 const azureURLs = [
   "https://bens-resume-challenge-visits.azurewebsites.net/api/get-visits",
