53f800ee8fc434f1284eb85c2a9340de5c048afb
Ben Parsons <bp_coding@yahoo.com.au>
2024-05-08 22:15:17 +0800

    fixing beanstalk

--- docker-setup/worked but couldn't link to ecs/dock.bat
+++
@@ -1,4 +0,0 @@
-docker run -dp 8000:80 --name resume-challenge-container resume-challenge-image:latest
-
-@REM docker build -t resume-challenge .
-@REM docker run -p 8080:80 resume-challenge
--- docker-setup/worked but couldn't link to ecs/generate-config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-envsubst '$BACKEND_HOST' < /etc/nginx/conf.d/custom-nginx.template > /etc/nginx/conf.d/default.conf;
-exec nginx -g "daemon off;";
